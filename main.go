3b058ae12d6b1b3ded22a6ad3723b46359c684e2
xavier-kong <kongxianchengxavier@gmail.com>
2023-05-05 21:50:50 +0800

    add filter of existing events

--- main.go
+++ main.go
@@ -5,25 +5,60 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
 	"github.com/gocolly/colly"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
 )
 
 type Event struct {
+	id int
 	name string
 	timestampSeconds int
 	headline string
 	url string
+	org string
 }
 
 func main() {
+	existingEvents := createExistingEventsMap()
+	newEvents := make([]Event, 0)
+
+	for _, event := range fetchEvents() {
+		if _, exists := existingEvents[event.name]; !exists {
+			newEvents = append(newEvents, event)
+		}
+	}
+
+	writeNewEventsToDb(newEvents)
+}
+
+func convertUrlToEventName(url string) string {
+	res := strings.ReplaceAll(url, "/event/ufc", "UFC")
+	res = strings.ReplaceAll(res, "-", " ")
+	c := cases.Title(language.English, cases.NoLower)
+	return c.String(res)
+}
+
+func createExistingEventsMap() map[string]bool {
+	m := make(map[string]bool)
+
+	// fetch all future from db
+
+	return m
+}
+
+func writeNewEventsToDb(events []Event) {
+
+}
+
+func fetchEvents() []Event {
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.ufc.com"),
 	)
 
 	todaySecs := int(time.Now().UnixMilli() / 1000)
+
+	events := make([]Event, 0)
 
 	c.OnHTML(".c-card-event--result__info", func(e *colly.HTMLElement) {
 		eventHeadline := e.ChildText(".c-card-event--result__headline")
@@ -52,17 +87,13 @@ func main() {
 			headline: eventHeadline,
 			timestampSeconds: timestampMs,
 			url: eventUrl,
+			org: "UFC",
 		}
 
-		fmt.Println(event)
+		events = append(events, event)
 	})
 
 	c.Visit("https://www.ufc.com/events#events-list-upcoming")
-}
 
-func convertUrlToEventName(url string) string {
-	res := strings.ReplaceAll(url, "/event/ufc", "UFC")
-	res = strings.ReplaceAll(res, "-", " ")
-	c := cases.Title(language.English, cases.NoLower)
-	return c.String(res)
+	return events;
 }
